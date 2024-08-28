45a3cb6017ce5ff48ef08d908e094f3f27839670
Rami Kader <ramy.kader.rk@gmail.com>
2024-08-28 02:27:48 +0200

    assert and prep db - podcasts

--- podcas/podcas/datastore.py
+++ podcas/podcas/datastore.py
@@ -10,36 +10,96 @@ class DataStore:
     CATEGORY_TAB = 'categories'
     PODCAST_TAB = 'podcasts'
     REVIEW_TAB = 'reviews'
-    CATEGORY_EMBEDS = 'v_cat'
-    PODCAST_EMBEDS = 'v_pod'
+    CATEGORY_EMBEDS = 'catV'
+    PODCAST_EMBEDS = 'podV'
+    META_TAB = 'META'
 
     _logger = logging.getLogger(f"{__name__}.{__qualname__}")
 
-    def __init__(self, path: str):
+    def __init__(
+            self,
+            path: str,
+            category_model: str = 'all-MiniLM-L6-v2',
+            review_model: str ='distiluse-base-multilingual-cased-v1',
+            podcast_model: str ='distiluse-base-multilingual-cased-v1'
+    ):
         self.file = abspath(path)
-        with self._conn() as db_conn:
-            if not DataStore._assert_categories(db_conn):
-                Embedder._embed_categories(
-                    db_conn,
-                    DataStore.CATEGORY_TAB,
-                    DataStore.CATEGORY_EMBEDS)
-            if not DataStore._assert_reviews(db_conn):
-                Embedder._embed_reviews(db_conn, DataStore.REVIEW_TAB)
-            Embedder._embed_podcasts(db_conn, DataStore.PODCAST_TAB)
+        self.embedder = Embedder(
+            category_model = category_model,
+            review_model = review_model,
+            podcast_model = podcast_model
+        )
+        self._init_db()
+
 
     @contextmanager
     def _conn(self) -> Generator[duckdb.DuckDBPyConnection, None, None]:
-        connection = duckdb.connect(
-            self.file,
-            config = {'threads': 1}
-        )
+        connection = duckdb.connect(self.file, config = {'threads': 1})
         connection.load_extension('vss')
         connection.execute("SET hnsw_enable_experimental_persistence = true")
 
         try: yield connection
         except Exception as e:
-            print(f'Could not obtain database connection curosr: {e}')
+            DataStore._logger.error(
+                f'Could not obtain database connection curosr: {e}',
+                exc_info=True
+            )
         finally: connection.close()
+
+    def _init_db(self) -> None:
+        with self._conn() as conn:
+            if not DataStore._assert_meta(conn, self.embedder.models):
+                DataStore._record_meta(conn, self.embedder.models)
+                DataStore._prep_categories(conn)
+                DataStore._prep_reviews(conn)
+                DataStore._prep_podcasts(conn)
+                self.embedder.embed_categories(
+                    conn,
+                    DataStore.CATEGORY_TAB,
+                    DataStore.CATEGORY_EMBEDS)
+                self.embedder.embed_reviews(conn, DataStore.REVIEW_TAB)
+                self.embedder.embed_podcasts(
+                    conn,
+                    DataStore.PODCAST_TAB,
+                    DataStore.PODCAST_EMBEDS)
+            else:
+                if not DataStore._assert_categories(conn):
+                    DataStore._prep_categories(conn)
+                    self.embedder.embed_categories(
+                        conn,
+                        DataStore.CATEGORY_TAB,
+                        DataStore.CATEGORY_EMBEDS)
+                if not DataStore._assert_reviews(conn):
+                    DataStore._prep_reviews(conn)
+                    self.embedder.embed_reviews(conn, DataStore.REVIEW_TAB)
+                if not DataStore._assert_podcasts(conn):
+                    DataStore._prep_podcasts(conn)
+                    self.embedder.embed_podcasts(
+                        conn,
+                        DataStore.PODCAST_TAB,
+                        DataStore.PODCAST_EMBEDS)
+
+    @staticmethod
+    def _assert_meta(
+            conn: duckdb.DuckDBPyConnection,
+            models: dict[str, str]
+    ) -> bool:
+        table_exists = conn.execute(f"""
+            SELECT COUNT(table_name)
+            FROM information_schema.tables
+            WHERE table_name = ?
+        """, [DataStore.META_TAB]).fetchone()
+
+        if not table_exists or table_exists[0] < 1: return False
+
+        models_exist = conn.execute(f"""
+        SELECT COUNT(*) FROM {DataStore.META_TAB}
+        WHERE mod_cat = $category
+        AND mod_rev = $review
+        AND mod_pod = $podcast
+        """, models).fetchone()
+
+        return models_exist is not None and models_exist[0] == 1
 
     @staticmethod
     def _assert_categories(conn: duckdb.DuckDBPyConnection) -> bool:
@@ -57,7 +117,93 @@ class DataStore:
         SELECT COUNT(column_name)
         FROM information_schema.columns
         WHERE table_name = '{DataStore.REVIEW_TAB}'
-        AND column_name IN ('vec_title', 'vec_content', 'vec_aggregated')
+        AND column_name IN ('rev_id', 'vec_title', 'vec_content', 'vec_aggregated')
         """
         result, *_ = conn.sql(query).fetchall()
-        return result[0] == 3
+        return result[0] == 4
+
+    @staticmethod
+    def _assert_podcasts(conn: duckdb.DuckDBPyConnection) -> bool:
+        query = f"""
+        SELECT COUNT(table_name)
+        FROM information_schema.tables
+        WHERE table_name = '{DataStore.PODCAST_EMBEDS}'
+        """
+        result, *_ = conn.sql(query).fetchall()
+        return result[0] == 1
+
+    @staticmethod
+    def _record_meta(
+            conn: duckdb.DuckDBPyConnection,
+            models: dict[str, str]
+    ) -> None:
+        model_names = [
+            models['category'],
+            models['review'],
+            models['podcast']
+        ]
+
+        DataStore._with_transaction(conn, [
+            f"DROP TABLE IF EXISTS {DataStore.META_TAB}",
+            f"""
+            CREATE TABLE {DataStore.META_TAB}(
+                mod_cat VARCHAR,
+                mod_rev VARCHAR,
+                mod_pod VARCHAR
+            )""",
+            f"""
+            INSERT INTO {DataStore.META_TAB}
+            VALUES ('{"', '".join(model_names)}')"""
+        ])
+
+    @staticmethod
+    def _prep_categories(conn: duckdb.DuckDBPyConnection) -> None:
+        DataStore._with_transaction(conn, [
+            f"""
+            UPDATE {DataStore.CATEGORY_TAB}
+            SET category = LOWER(category)"""
+        ])
+
+    @staticmethod
+    def _prep_reviews(conn: duckdb.DuckDBPyConnection) -> None:
+        DataStore._with_transaction(conn, [
+            f"""
+            ALTER TABLE {DataStore.REVIEW_TAB}
+            DROP COLUMN IF EXISTS rev_id""",
+            f"""
+            ALTER TABLE {DataStore.REVIEW_TAB}
+            ADD COLUMN rev_id INTEGER""",
+            f"""
+            UPDATE {DataStore.REVIEW_TAB}
+            SET rev_id = seq.seq_num
+            FROM (
+                SELECT ROW_NUMBER() OVER () AS seq_num, rowid
+                FROM {DataStore.REVIEW_TAB}
+            ) AS seq
+            WHERE {DataStore.REVIEW_TAB}.rowid = seq.rowid"""
+        ])
+
+    @staticmethod
+    def _prep_podcasts(conn: duckdb.DuckDBPyConnection) -> None:
+        DataStore._with_transaction(conn, [
+            f"""
+            UPDATE {DataStore.PODCAST_TAB}
+            SET title = LOWER(title),
+                author = LOWER(author)
+            """
+        ])
+
+    @staticmethod
+    def _with_transaction(
+            conn: duckdb.DuckDBPyConnection,
+            queries: list[str]
+    ) -> None:
+        conn.begin()
+        try:
+            for query in queries: conn.execute(query)
+            conn.commit()
+        except Exception as e:
+            DataStore._logger.error(f'Error occured during transaction: {e}', exc_info=True)
+            DataStore._logger.debug('Rolling back...')
+            conn.rollback()
+            raise e
--- podcas/podcas/embedder.py
+++ podcas/podcas/embedder.py
@@ -3,29 +3,52 @@ from duckdb import DuckDBPyConnection
 from sentence_transformers import SentenceTransformer
 
 
+# TODO: decouple Embedder from DB
+# and move db logic to DataStore
 class Embedder:
     _logger = getLogger(f"{__name__}.{__qualname__}")
 
-    @staticmethod
-    def _embed_categories(
+    def __init__(
+            self,
+            category_model: str,
+            review_model: str,
+            podcast_model: str
+    ):
+        self.category_embedder = SentenceTransformer(category_model)
+        self.review_embedder = SentenceTransformer(review_model)
+        self.podcast_embedder = SentenceTransformer(podcast_model)
+        self.models = {
+            'category': category_model,
+            'review': review_model,
+            'podcast': podcast_model
+        }
+
+    def embed_categories(
+            self,
             conn: DuckDBPyConnection,
             table: str,
             embedded_table: str
     ) -> None:
         query = f'SELECT DISTINCT category FROM {table}'
-        categories: list[str] = [result[0] for result in conn.sql(query).fetchall()]
-        encoder = SentenceTransformer('all-mpnet-base-v2')
+        categories: list[str] = [
+            result[0]
+            for result in conn.sql(query).fetchall()
+        ]
 
         Embedder._logger.info("Embedding categories...")
-        embeddings = encoder.encode(categories, show_progress_bar=True)
+        embeddings = self.category_embedder.encode(
+            categories,
+            show_progress_bar=True
+        )
         category_index = {
-            category: embedding
+            category: embedding.tolist()
             for category, embedding in zip(categories, embeddings)
         }
-        dim = category_index[categories[0]].shape[0]
+        dim = len(category_index[categories[0]])
 
         conn.begin()
         try:
+            conn.sql(f"DROP TABLE IF EXISTS {embedded_table}")
             conn.sql(f"""
             CREATE TABLE {embedded_table}(
                 name VARCHAR PRIMARY KEY,
@@ -33,12 +56,14 @@ class Embedder:
             )
             """)
 
-            for category, vector in category_index.items():
-                vector_str = ', '.join([str(x) for x in vector.tolist()])
-                conn.sql(f"""
-                INSERT INTO {embedded_table}
-                VALUES ('{category}', [{vector_str}])
-                """)
+            # # TODO: partition or parallelize over cursors
+            conn.executemany(
+                f"INSERT INTO {embedded_table} VALUES (?, ?)",
+                [
+                    [category, vector]
+                    for category, vector in category_index.items()
+                ]
+            )
 
             conn.sql(f"""
             CREATE INDEX idx_cat
@@ -49,88 +74,69 @@ class Embedder:
             conn.commit()
 
         except Exception as e:
-            Embedder._logger.error(f'Error occured during transaction: {e}')
-            Embedder._logger.error('Rolling back...')
+            Embedder._logger.error(f'Error occured during transaction: {e}', exc_info=True)
+            Embedder._logger.debug('Rolling back...')
             conn.rollback()
+            raise e
 
-    @staticmethod
-    def _embed_reviews(conn: DuckDBPyConnection, table: str) -> None:
+    def embed_reviews(self, conn: DuckDBPyConnection, table: str) -> None:
+        reviews: list[tuple[str, str, str]] = conn.sql(f"""
+        SELECT rev_id, title, content
+        FROM {table}
+        """).fetchall()
+        ids = [idx for idx, _, _ in reviews]
+        aggregated = [
+            f'TITLE:{title} - CONTENT:{content}'
+            for _, title, content in reviews
+        ]
+
+        Embedder._logger.info("Embedding review titles...")
+        title_embeddings = self.review_embedder.encode(
+            [title for _, title, _ in reviews],
+            show_progress_bar = True
+        )
+
+        Embedder._logger.info("Embedding review content...")
+        content_embeddings = self.review_embedder.encode(
+            [content for _, _, content in reviews],
+            show_progress_bar = True
+        )
+
+        Embedder._logger.info("Embedding full reviews...")
+        review_embeddings = self.review_embedder.encode(
+            aggregated,
+            show_progress_bar = True
+        )
+        dim = title_embeddings[0].shape[0]
+
+        title_case_stmts = [
+            f"""WHEN rev_id = {rev_id}
+            THEN [{', '.join([str(x) for x in title_emb.tolist()])}]"""
+            for rev_id, title_emb in zip(ids, title_embeddings)
+        ]
+        cont_case_stmts = [
+            f"""WHEN rev_id = {rev_id}
+            THEN [{', '.join([str(x) for x in cont_emb.tolist()])}]"""
+            for rev_id, cont_emb in zip(ids, content_embeddings)
+        ]
+        agg_case_stmts = [
+            f"""WHEN rev_id = {rev_id}
+            THEN [{', '.join([str(x) for x in agg_emb.tolist()])}]"""
+            for rev_id, agg_emb in zip(ids, review_embeddings)
+        ]
+
         conn.begin()
         try:
-            conn.sql(f"""
-            ALTER TABLE {table}
-            ADD COLUMN rev_id INTEGER
-            """)
+            conn.sql(f"ALTER TABLE {table} DROP COLUMN IF EXISTS vec_title")
+            conn.sql(f"ALTER TABLE {table} ADD COLUMN vec_title FLOAT[{dim}]")
 
-            conn.sql(f"""
-            UPDATE {table}
-            SET rev_id = seq.seq_num
-            FROM (
-                SELECT ROW_NUMBER() OVER () AS seq_num, rowid
-                FROM {table}
-            ) AS seq
-            WHERE {table}.rowid = seq.rowid
-            """)
+            conn.sql(f"ALTER TABLE {table} DROP COLUMN IF EXISTS vec_content")
+            conn.sql(f"ALTER TABLE {table} ADD COLUMN vec_content FLOAT[{dim}]")
 
-            reviews: list[str] = conn.sql(f"""
-            SELECT rev_id, title, content
-            FROM {table}
-            """).fetchall()
-            ids = [idx for idx, _, _ in reviews]
-            aggregated = [
-                f'TITLE:{title} - CONTENT:{content}'
-                for _, title, content in reviews
-            ]
+            conn.sql(f"ALTER TABLE {table} DROP COLUMN IF EXISTS vec_aggregated")
+            conn.sql(f"ALTER TABLE {table} ADD COLUMN vec_aggregated FLOAT[{dim}]")
 
-            encoder = SentenceTransformer('distiluse-base-multilingual-cased-v1')
-            Embedder._logger.info("Embedding review titles...")
-            title_embeddings = encoder.encode(
-                [title for _, title, _ in reviews],
-                show_progress_bar = True
-            )
-
-            Embedder._logger.info("Embedding review content...")
-            content_embeddings = encoder.encode(
-                [content for _, _, content in reviews],
-                show_progress_bar = True
-            )
-
-            Embedder._logger.info("Embedding full reviews...")
-            review_embeddings = encoder.encode(aggregated, show_progress_bar = True)
-            dim = title_embeddings[0].shape[0]
-
-            conn.sql(f"""
-            ALTER TABLE {table}
-            ADD COLUMN vec_title FLOAT[{dim}]
-            """)
-            conn.sql(f"""
-            ALTER TABLE {table}
-            ADD COLUMN vec_content FLOAT[{dim}]
-            """)
-            conn.sql(f"""
-            ALTER TABLE {table}
-            ADD COLUMN vec_aggregated FLOAT[{dim}]
-            """)
-
-            title_case_stmts = [
-                f"""WHEN rev_id = {rev_id}
-                THEN [{', '.join([str(x) for x in title_emb.tolist()])}]"""
-                for rev_id, title_emb in zip(ids, title_embeddings)
-            ]
-            cont_case_stmts = [
-                f"""WHEN rev_id = {rev_id}
-                THEN [{', '.join([str(x) for x in cont_emb.tolist()])}]"""
-                for rev_id, cont_emb in zip(ids, content_embeddings)
-            ]
-            agg_case_stmts = [
-                f"""WHEN rev_id = {rev_id}
-                THEN [{', '.join([str(x) for x in agg_emb.tolist()])}]"""
-                for rev_id, agg_emb in zip(ids, review_embeddings)
-            ]
-
-            # not great for big tables
-            # to be parallelised over partitions and/or cursors
-            # a column-oriented db would ve been much better
+            # TODO: partition or parallelize over cursors
             conn.sql(f"""
             UPDATE {table}
             SET vec_title = CASE
@@ -163,9 +169,14 @@ class Embedder:
             conn.commit()
 
         except Exception as e:
-            Embedder._logger.error(f'Error occured during transaction: {e}')
-            Embedder._logger.error('Rolling back...')
-            conn.rollback
+            Embedder._logger.error(f'Error occured during transaction: {e}', exc_info=True)
+            Embedder._logger.debug('Rolling back...')
+            conn.rollback()
+            raise e
 
-    @staticmethod
-    def _embed_podcasts(conn: DuckDBPyConnection, table: str) -> None: ...
+    def embed_podcasts(
+            self,
+            conn: DuckDBPyConnection,
+            table: str,
+            embedded_table: str
+    ) -> None: ...
