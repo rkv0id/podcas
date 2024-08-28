24eb99e13f4a021df20a88091f35b39013515bc4
Rami Kader <ramy.kader.rk@gmail.com>
2024-08-29 01:20:15 +0200

    add review search for fanciness

---
+++ podcas/podcas/reviewsearch.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+from threading import Lock
+from logging import getLogger
+
+from .datastore import DataStore
+from .embedder import Embedder
+
+class ReviewSearch:
+    __instance = None
+    __lock = Lock()
+    _logger = getLogger(f"{__name__}.{__qualname__}")
+
+    def __new__(cls, *args, **kwargs):
+        with cls.__lock:
+            if not cls.__instance:
+                cls.__instance = super(ReviewSearch, cls).__new__(cls)
+        return cls.__instance
+
+    def __init__(
+            self,
+            path: str, *,
+            category_model: str = 'all-MiniLM-L6-v2',
+            review_model: str = 'distiluse-base-multilingual-cased-v1',
+            podcast_model: str = 'distiluse-base-multilingual-cased-v1',
+            # TODO
+            rank_boost: bool = False
+    ):
+        self.source = path
+        self._embedder = Embedder(
+            category_model = category_model,
+            review_model = review_model,
+            podcast_model = podcast_model
+        )
+        self._db = DataStore(path, self._embedder)
+
+    # TODO
+    def by_score(self, min: float, max: float = 5.) -> ReviewSearch: ...
+    def by_query(self, query: str) -> ReviewSearch: ...
