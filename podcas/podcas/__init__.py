52b6b1a4f47e5751a843fc2db3d7c7f0afc82b69
Rami Kader <ramy.kader.rk@gmail.com>
2024-08-30 06:05:04 +0200

    random test case

--- podcas/podcas/__init__.py
+++ podcas/podcas/__init__.py
@@ -3,5 +3,17 @@ import duckdb
 duckdb.install_extension("vss")
 
 # testing start
+default_embedder = 'multi-qa-MiniLM-L6-cos-v1'
 from .podcastsearch import PodcastSearch
-PodcastSearch("../data/mid.db")
+(
+    PodcastSearch()
+        .load(source="../data/mid.db")
+        .using(
+            category_model = default_embedder,
+            podcast_model = default_embedder,
+            review_model = default_embedder)
+        .top(4)
+        .by_rating(3)
+        .by_category('fiction')
+        .get()
+)
